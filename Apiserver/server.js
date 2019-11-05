77233642ddb6e982e88ba63ad450c93f10fe1bb1
Yoovin Shim <yoovin@Yoovin-MacBookPro.local>
2019-11-05 19:24:09 +0900

    Project 환경 설정

---
+++ Apiserver/server.js
@@ -0,0 +1,10 @@
+// 백엔드 서버입니다.
+
+const app = require('express')()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res)=>{
+    res.send("Hello world!")
+})
+
+app.listen(port, console.log(`Backend서버 ${port}에서 돌아가는중!`))
